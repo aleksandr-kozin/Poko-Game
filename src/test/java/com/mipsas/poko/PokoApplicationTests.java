28eafd440a1b0e88fbefc381779794c19ee828f6
aleksandr.k <aleksandr.k@mipsas.com>
2022-06-13 22:38:32 +0300

    Init commit

---
+++ src/test/java/com/mipsas/poko/PokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mipsas.poko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
