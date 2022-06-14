eadf01a1d40f8bd2b0d45de55db39c96e9d3e6d0
aleksandr.k <aleksandr.k@mipsas.com>
2022-06-14 18:11:57 +0300

    Added UserRepository#findJwtUserByEmail() method to receive user with credentials

--- src/main/java/com/mipsas/poko/data/repository/UserRepository.java
+++ src/main/java/com/mipsas/poko/data/repository/UserRepository.java
@@ -1,7 +1,16 @@
 package com.mipsas.poko.data.repository;
 
 import com.mipsas.poko.data.entity.UserEntity;
+import com.mipsas.poko.security.jwt.JwtUser;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    @Query("""
+            SELECT new com.mipsas.poko.security.jwt.JwtUser(u.nickName, c.password, u.role.name, u.status) 
+            FROM CredentialEntity c JOIN c.user u
+            """)
+    Optional<JwtUser> findJwtUserByEmail(String email);
 }
