c1d5713f715baf45b07fe5bbbfd31e4a4e021d32
aleksandr.k <aleksandr.k@mipsas.com>
2022-06-15 20:05:47 +0300

    Added several columns to user_locations and meta_data tables

--- src/main/java/com/mipsas/poko/data/entity/UserLocationEntity.java
+++ src/main/java/com/mipsas/poko/data/entity/UserLocationEntity.java
@@ -1,9 +1,11 @@
 package com.mipsas.poko.data.entity;
 
+import java.time.Instant;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -17,6 +19,13 @@ public class UserLocationEntity extends BaseEntity<Long> {
 
     private Double latitude;
     private Double longitude;
+    private String country;
+    private String city;
+    private String postal;
+    private String state;
+
+    @Builder.Default
+    private Instant date = Instant.now();
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
