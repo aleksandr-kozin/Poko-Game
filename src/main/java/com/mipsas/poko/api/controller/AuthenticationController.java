e03007e3c696940098adeff9ef08c745daaba831
aleksandr.k <aleksandr.k@mipsas.com>
2022-06-20 10:16:09 +0300

    Added access annotations

--- src/main/java/com/mipsas/poko/api/controller/AuthenticationController.java
+++ src/main/java/com/mipsas/poko/api/controller/AuthenticationController.java
@@ -10,6 +10,7 @@ import com.mipsas.poko.api.model.response.SignInResponse;
 import com.mipsas.poko.api.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,18 +23,21 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
+    @PermitAll
     @Operation(summary = "Register new user")
     @PostMapping(REGISTRATION)
     public void registration(@RequestBody @Valid UserRegisterRequest request) {
         authenticationService.registerUser(request);
     }
 
+    @PermitAll
     @Operation(summary = "User sign in")
     @PostMapping(SIGN_IN)
     public SignInResponse signIn(@RequestBody @Valid SignInRequest request) {
         return authenticationService.signIn(request);
     }
 
+    @PermitAll
     @Operation(summary = "User sign out")
     @PostMapping(SIGN_OUT + "/{userId}")
     public void signOut(@PathVariable Long userId, HttpServletRequest request) {
--- src/main/resources/database/test-data.sql
+++ src/main/resources/database/test-data.sql
@@ -1,3 +1,3 @@
-INSERT INTO users VALUES (1, 'Jonny', 'John', 'Rembo', 'NOT_ACTIVE', 'USER');
+INSERT INTO users VALUES (1, 'Jonny', 'John', 'Rembo', 'NOT_ACTIVE', 'ADMIN');
 
 INSERT INTO credentials VALUES (1, 'johnrembo@mail.com', '$2a$04$JLauWJ2GYfvH6U0384lbD.tegCcnFoWD1NxzJ2RWGw3kI.amsTun6', 1); -- password: password
