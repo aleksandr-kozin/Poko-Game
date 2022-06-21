a7ae014a9839a11abe7a30bdf2aff75f26e2b0c2
aleksandr.k <aleksandr.k@mipsas.com>
2022-06-21 23:03:45 +0300

    Added AuthenticationService#resetPassword() method

--- src/main/java/com/mipsas/poko/api/service/AuthenticationService.java
+++ src/main/java/com/mipsas/poko/api/service/AuthenticationService.java
@@ -1,5 +1,6 @@
 package com.mipsas.poko.api.service;
 
+import com.mipsas.poko.api.model.request.ResetPasswordRequest;
 import com.mipsas.poko.api.model.request.SignInRequest;
 import com.mipsas.poko.api.model.request.UserRegisterRequest;
 import com.mipsas.poko.api.model.response.SignInResponse;
@@ -9,4 +10,5 @@ public interface AuthenticationService {
     SignInResponse signIn(SignInRequest request);
     void registerUser(UserRegisterRequest request);
     void signOut(HttpServletRequest request);
+    void resetPassword(ResetPasswordRequest request);
 }
--- src/main/java/com/mipsas/poko/api/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/mipsas/poko/api/service/impl/AuthenticationServiceImpl.java
@@ -1,18 +1,19 @@
 package com.mipsas.poko.api.service.impl;
 
 import static com.mipsas.poko.api.exception.ErrorStatus.*;
+import com.mipsas.poko.api.model.request.ResetPasswordRequest;
 import com.mipsas.poko.api.model.request.SignInRequest;
 import com.mipsas.poko.api.model.request.UserRegisterRequest;
 import com.mipsas.poko.api.model.response.SignInResponse;
 import com.mipsas.poko.api.service.AuthenticationService;
+import com.mipsas.poko.api.service.CredentialService;
+import com.mipsas.poko.api.service.UserService;
 import static com.mipsas.poko.common.enums.UserAuthority.USER;
 import static com.mipsas.poko.common.enums.UserStatus.ACTIVE;
 import static com.mipsas.poko.common.enums.UserStatus.NOT_ACTIVE;
 import com.mipsas.poko.data.entity.CredentialEntity;
 import com.mipsas.poko.data.entity.JwtBlackListEntity;
 import com.mipsas.poko.data.entity.UserEntity;
-import com.mipsas.poko.data.repository.CredentialRepository;
-import com.mipsas.poko.data.repository.UserRepository;
 import com.mipsas.poko.security.jwt.JwtUser;
 import com.mipsas.poko.security.jwt.UserAndClaims;
 import com.mipsas.poko.security.service.JwtBlackListService;
@@ -30,10 +31,10 @@ import org.springframework.stereotype.Service;
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final JwtBlackListService jwtBlackListService;
     private final JwtService jwtService;
-    private final UserRepository userRepository;
-    private final CredentialRepository credentialRepository;
+    private final UserService userService;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final CredentialService credentialService;
 
     @Override
     public SignInResponse signIn(SignInRequest request) {
@@ -46,13 +47,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public void registerUser(UserRegisterRequest request) {
-        userRepository.findByNickName(request.nickName())
-                .ifPresent(user -> EXISTS_USER.throwException());
+        userService.throwExceptionIfExists(request.nickName());
+        credentialService.throwExceptionIfExists(request.email());
 
-        credentialRepository.findByEmail(request.email())
-                .ifPresent(c -> EXISTS_CREDENTIAL.throwException());
-
-        UserEntity savedUser = userRepository.save(UserEntity.builder()
+        UserEntity savedUser = userService.save(UserEntity.builder()
                 .nickName(request.nickName())
                 .firstName(request.firstName())
                 .lastName(request.lastName())
@@ -60,7 +58,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .status(NOT_ACTIVE)
                 .build());
 
-        credentialRepository.save(CredentialEntity.builder()
+        credentialService.save(CredentialEntity.builder()
                 .user(savedUser)
                 .email(request.email())
                 .password(passwordEncoder.encode(request.password()))
@@ -72,16 +70,28 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         String token = jwtService.resolveToken(request);
         UserAndClaims userNameWithClaims = jwtService.getUserNameWithClaims(token);
 
-        userRepository.findByNickName(userNameWithClaims.getUserName())
-                .ifPresentOrElse(user -> {
-                    user.setStatus(NOT_ACTIVE);
-                    userRepository.save(user);
-                }, NOT_EXISTS_USER::throwException);
+        userService.changeUserStatus(userNameWithClaims.getUserName(), NOT_ACTIVE);
 
         jwtBlackListService.addToBlackList(JwtBlackListEntity.builder()
                 .token(token)
                 .expirationDate(jwtService.getExpirationDate(token))
                 .build());
+    }
+
+    @Override
+    public void resetPassword(ResetPasswordRequest request) {
+        CredentialEntity credential = credentialService.getByUserId(request.userId());
+
+        if (passwordEncoder.matches(request.oldPassword(), credential.getPassword())) {
+            if (request.newPassword().equals(request.repeatPassword())) {
+                credential.setPassword(passwordEncoder.encode(request.newPassword()));
+                credentialService.save(credential);
+            } else {
+                PASSWORDS_NOT_EQUAL.throwException();
+            }
+        } else {
+            INCORRECT_PASSWORD.throwException();
+        }
     }
 
     private Authentication authenticate(SignInRequest request) {
@@ -99,12 +109,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private JwtUser activateUser(Authentication auth) {
         JwtUser jwtUser = (JwtUser) auth.getPrincipal();
-
-        userRepository.findByNickName(jwtUser.getNickName())
-                .ifPresent(user -> {
-                    user.setStatus(ACTIVE);
-                    userRepository.save(user);
-                });
+        userService.changeUserStatus(jwtUser.getNickName(), ACTIVE);
 
         return jwtUser;
     }
